94682271ac593e6673656f9c22ceb0904c5cd0f0
sodmaq <sodmaq@gmail.com>
2024-05-28 10:49:35 +0100

    added features component

---
+++ src/components/Features.jsx
@@ -0,0 +1,21 @@
+function Features() {
+  return (
+    <div className="my-24 md:px-14 px-4 max-w-screen-2xl mx-auto">
+      <div>
+        <div className="lg:w-1/4">
+          <h3 className="text-3xl text-primary font-bold lg:w-1/2 mb-3">
+            We are better than Others
+          </h3>
+          <p className="text-base text-tertiary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            voluptate quia similique autem eum quasi quis quo exercitationem
+            soluta optio eius perspiciatis, odio aperiam doloremque repudiandae
+            explicabo laudantium laborum error.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Features;
