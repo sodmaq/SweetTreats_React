50938d6779fcc008d204a0f2f37afede1f63850d
sodmaq <sodmaq@gmail.com>
2024-05-28 22:14:06 +0100

    added about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,20 @@ function About() {
         <div className="md:w-1/2">
           <img src={aboutImg} alt="" />
         </div>
+
+        {/* about content */}
+        <div>
+          <h2 className="md:text-5 text-3xl font-bold text-primary mb-5 leading-normal">
+            we have been improving our product <span>for many years</span>
+          </h2>
+          <p className="text-tertiary text-lg mb-7">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque
+            doloremque quia voluptates inventore totam quo? Optio, quos sint.
+            Nisi est totam aspernatur molestias saepe distinctio exercitationem
+            quaerat natus perspiciatis libero.
+          </p>
+          <button className="btnPrimary">Get Started</button>
+        </div>
       </div>
     </div>
   );
