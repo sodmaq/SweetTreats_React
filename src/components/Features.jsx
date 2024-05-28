5063b03bf6682d0ed235cb8612462eb18d8b49a4
sodmaq <sodmaq@gmail.com>
2024-05-28 15:25:06 +0100

    added about component

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,9 @@
+import featuredImg from "../assets/featuredImg.png";
+
 function Features() {
   return (
     <div className="my-24 md:px-14 px-4 max-w-screen-2xl mx-auto">
-      <div>
+      <div className="flex flex-col lg:flex-row justify-between items-start gap-10">
         <div className="lg:w-1/4">
           <h3 className="text-3xl text-primary font-bold lg:w-1/2 mb-3">
             We are better than Others
@@ -12,6 +14,35 @@ function Features() {
             soluta optio eius perspiciatis, odio aperiam doloremque repudiandae
             explicabo laudantium laborum error.
           </p>
+        </div>
+        {/* feature card */}
+        <div className="w-full lg:w-3/4">
+          <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 items-start md:gap-12 gap-8">
+            <div className="bg-[rgba(255,255,255,0.24)] rounded-[35px] h-96 shadow-3xl p-8 items-center flex justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer">
+              <div>
+                <img src={featuredImg} alt="" />
+                <h5 className="text-2xl font-semibold text-primary px-5 text-center mt-5">
+                  convenient study schedule
+                </h5>
+              </div>
+            </div>
+            <div className="bg-[rgba(255,255,255,0.24)] rounded-[35px] h-96 shadow-3xl p-8 items-center flex justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer md:mt-16">
+              <div>
+                <img src={featuredImg} alt="" />
+                <h5 className="text-2xl font-semibold text-primary px-5 text-center mt-5">
+                  convenient study schedule
+                </h5>
+              </div>
+            </div>
+            <div className="bg-[rgba(255,255,255,0.24)] rounded-[35px] h-96 shadow-3xl p-8 items-center flex justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer">
+              <div>
+                <img src={featuredImg} alt="" />
+                <h5 className="text-2xl font-semibold text-primary px-5 text-center mt-5">
+                  convenient study schedule
+                </h5>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
