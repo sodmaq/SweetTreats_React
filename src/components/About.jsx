6ca4e82e0ed0ebe5a4d21f34bce74d11d34a0b83
sodmaq <sodmaq@gmail.com>
2024-05-29 06:55:37 +0100

    added about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,39 @@
 import aboutImg from "../assets/aboutImg.png";
 function About() {
   return (
-    <div className="md:px-14 p-4 max-w-s mx-auto">
-      <div>
+    <div className="md:px-14 p-4 max-w-s mx-auto space-y-10">
+      <div className="flex flex-col md:flex-row justify-between items-center gap-8">
         <div className="md:w-1/2">
           <img src={aboutImg} alt="" />
         </div>
 
         {/* about content */}
-        <div>
+        <div className="md:w-2/5">
           <h2 className="md:text-5 text-3xl font-bold text-primary mb-5 leading-normal">
-            we have been improving our product <span>for many years</span>
+            we have been improving our product{" "}
+            <span className="text-secondary">for many years</span>
+          </h2>
+          <p className="text-tertiary text-lg mb-7">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque
+            doloremque quia voluptates inventore totam quo? Optio, quos sint.
+            Nisi est totam aspernatur molestias saepe distinctio exercitationem
+            quaerat natus perspiciatis libero.
+          </p>
+          <button className="btnPrimary">Get Started</button>
+        </div>
+      </div>
+
+      {/* second part */}
+      <div className="flex flex-col md:flex-row-reverse justify-between items-center gap-8">
+        <div className="md:w-1/2">
+          <img src={aboutImg} alt="" />
+        </div>
+
+        {/* about content */}
+        <div className="md:w-2/5">
+          <h2 className="md:text-5 text-3xl font-bold text-primary mb-5 leading-normal">
+            we have been improving our product{" "}
+            <span className="text-secondary">for many years</span>
           </h2>
           <p className="text-tertiary text-lg mb-7">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -6,39 +6,39 @@ function Features() {
       <div className="flex flex-col lg:flex-row justify-between items-start gap-10">
         <div className="lg:w-1/4">
           <h3 className="text-3xl text-primary font-bold lg:w-1/2 mb-3">
-            We are better than Others
+            We are better than others
           </h3>
           <p className="text-base text-tertiary">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem
             voluptate quia similique autem eum quasi quis quo exercitationem
             soluta optio eius perspiciatis, odio aperiam doloremque repudiandae
             explicabo laudantium laborum error.
           </p>
         </div>
-        {/* feature card */}
+        {/* Feature cards */}
         <div className="w-full lg:w-3/4">
           <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 items-start md:gap-12 gap-8">
-            <div className="bg-[rgba(255,255,255,0.24)] rounded-[35px] h-96 shadow-3xl p-8 items-center flex justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer">
+            <div className="bg-[rgba(255,255,255,0.24)] rounded-[35px] h-96 shadow-3xl p-8 flex justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer">
               <div>
-                <img src={featuredImg} alt="" />
+                <img src={featuredImg} alt="Feature 1" />
                 <h5 className="text-2xl font-semibold text-primary px-5 text-center mt-5">
-                  convenient study schedule
+                  Convenient study schedule
                 </h5>
               </div>
             </div>
-            <div className="bg-[rgba(255,255,255,0.24)] rounded-[35px] h-96 shadow-3xl p-8 items-center flex justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer md:mt-16">
+            <div className="bg-[rgba(255,255,255,0.24)] rounded-[35px] h-96 shadow-3xl p-8 flex justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer md:mt-16">
               <div>
-                <img src={featuredImg} alt="" />
+                <img src={featuredImg} alt="Feature 2" />
                 <h5 className="text-2xl font-semibold text-primary px-5 text-center mt-5">
-                  convenient study schedule
+                  Convenient study schedule
                 </h5>
               </div>
             </div>
-            <div className="bg-[rgba(255,255,255,0.24)] rounded-[35px] h-96 shadow-3xl p-8 items-center flex justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer">
+            <div className="bg-[rgba(255,255,255,0.24)] rounded-[35px] h-96 shadow-3xl p-8 flex justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer">
               <div>
-                <img src={featuredImg} alt="" />
+                <img src={featuredImg} alt="Feature 3" />
                 <h5 className="text-2xl font-semibold text-primary px-5 text-center mt-5">
-                  convenient study schedule
+                  Convenient study schedule
                 </h5>
               </div>
             </div>
