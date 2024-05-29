5256d61b5230d75979a993c0e502afe6bb65da94
sodmaq <sodmaq@gmail.com>
2024-05-29 23:24:37 +0100

    improved codebase

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,11 +6,8 @@ function Home() {
     <div className="md:px-12 p-4 max-w-screen-2xl mx-auto mt-24" id="home">
       <Banner
         banner={banner}
-        heading="Develop your skills without deligence"
-        subheading="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-        similique possimus culpa consequatur, suscipit laborum officiis
-        voluptatum enim molestias obcaecati ut placeat libero molestiae dolore
-        delectus dolorem asperiores fugit alias!"
+        heading="Adejoke Bliss"
+        subheading="Welcome to the online portfolio of Yekini Zainub, a passionate baker, culinary enthusiast, and dedicated marketing student. With a flair for creating delectable cakes, savory small chops, rich cocoa treats, and crispy plantain chips, Yekini Zainub combines her culinary skills with marketing expertise to deliver exceptional products."
         btn1="Get Started"
         btn2="Discount"
       />
