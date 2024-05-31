f5260bd4152e28bcb6afa77ba0dd8be5e9c59205
sodmaq <sodmaq@gmail.com>
2024-05-31 11:02:39 +0100

    added images

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -50,7 +50,7 @@ function Pricing() {
     <div className="md:px-14 p-4 max-w-7xl mx-auto py-10" id="product">
       <div className="text-center mb-10">
         <h2 className="md:text-5xl text-3xl font-extrabold text-primary mb-2">
-        My Delicious Offerings
+          My Delicious Offerings
         </h2>
       </div>
       <div className="grid md:grid-cols-3 sm:grid-cols-2  grid-cols-2 gap-6">
@@ -73,7 +73,7 @@ function Pricing() {
             <div className="flex items-center justify-between px-4 py-2 bg-gray-900">
               <h1 className="text-lg font-bold text-white">{plan.price}</h1>
               <button className="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors duration-300 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">
-                Add to cart
+                Buy Now
               </button>
             </div>
           </div>
